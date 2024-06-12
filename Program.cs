ffc6b80a1397d609914f4fb6a0c06d148eaab1da
ninjadiestanding <ascendancee@gmail.com>
2024-06-12 14:15:58 +0300

    .

--- Program.cs
+++ Program.cs
@@ -6,3 +6,4 @@ Console.WriteLine(someBool == true && someBool == false); //true
 var FluentCalculator = new FluentCalculator();
 var result = FluentCalculator.one.plus.two.plus.three.minus.one.minus.two.minus.four; 
 Console.WriteLine(result.Value); //-1
+Console.ReadLine();
